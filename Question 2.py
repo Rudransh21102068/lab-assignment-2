ba07ad35050529fc3dd58e909a033470b8094444
Rudransh21102068 <104332012+Rudransh21102068@users.noreply.github.com>
2022-05-15 20:37:43 +0530

    Add files via upload

---
+++ Question 2.py
@@ -0,0 +1,8 @@
+name = 'RUDRANSH'
+sid = '21102068'
+department = 'CIVIL ENGINEERING'
+cgpa = '9.9'
+print("Hello, my name is ",name)
+print("My SID is ",sid)
+print("I am from ",department)
+print("I have secured CGPA of ",cgpa)
