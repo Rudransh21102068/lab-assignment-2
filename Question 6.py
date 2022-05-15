4ee926b15fe86c2ebc1bc97f89155b5ea951704f
Rudransh21102068 <104332012+Rudransh21102068@users.noreply.github.com>
2022-05-15 20:59:15 +0530

    Add files via upload

---
+++ Question 6.py
@@ -0,0 +1,23 @@
+# Count number of bits to be flipped
+# to convert A into B
+ 
+# Function that count set bits
+def countSetBits( n ):
+    count = 0
+    while n:
+        count += 1
+        n &= (n-1)
+    return count
+     
+# Function that return count of
+# flipped number
+def FlippedCount(a , b):
+ 
+    # Return count of set bits in
+    # a XOR b
+    return countSetBits(a^b)
+ 
+# Driver code
+a = 2
+b = 5
+print(FlippedCount(a, b))
