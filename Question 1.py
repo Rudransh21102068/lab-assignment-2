466bd7a33414ce810a82a73993d67dd731818d74
Rudransh21102068 <104332012+Rudransh21102068@users.noreply.github.com>
2022-05-15 20:42:39 +0530

    Add files via upload

---
+++ Question 1.py
@@ -0,0 +1,13 @@
+str = "Python is a case sensitive language"
+length = len(str)
+print(length)
+reverse = str[::-1]
+print(reverse)
+slices = str[10:26]
+print(slices)
+replaced = str.replace('a case sensitive','object oriented')
+print(replaced)
+search = str.find('a')
+print(search)
+spacing = str.replace(" ","")
+print(spacing)
